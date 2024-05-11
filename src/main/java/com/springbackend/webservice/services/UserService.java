7de0ba991fbde4b3176a60b4f14e7f9565be59ae
Seu <helielsouza01234@gmail.com>
2024-05-11 12:22:01 -0300

    Service layer, component registration and users/id endpoint

---
+++ src/main/java/com/springbackend/webservice/services/UserService.java
@@ -0,0 +1,26 @@
+package com.springbackend.webservice.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springbackend.webservice.entities.User;
+import com.springbackend.webservice.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findByID(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
