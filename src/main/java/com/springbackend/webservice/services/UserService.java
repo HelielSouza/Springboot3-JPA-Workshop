9d8718a7e0090889dc8db82416962e670867360b
Seu <helielsouza01234@gmail.com>
2024-05-18 14:07:31 -0300

    Exception handling FindById method

--- src/main/java/com/springbackend/webservice/services/UserService.java
+++ src/main/java/com/springbackend/webservice/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.springbackend.webservice.entities.User;
 import com.springbackend.webservice.repositories.UserRepository;
+import com.springbackend.webservice.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findByID(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
