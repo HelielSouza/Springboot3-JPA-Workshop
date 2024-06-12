c73eb49b4799ade13013501281ae6a67f1f7be8e
Seu <helielsouza01234@gmail.com>
2024-06-12 00:08:02 -0300

    Included PUT method to categories and tested all endpoints

--- src/main/java/com/springbackend/webservice/services/CategoryService.java
+++ src/main/java/com/springbackend/webservice/services/CategoryService.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 
 import com.springbackend.webservice.entities.Category;
 import com.springbackend.webservice.repositories.CategoryRepository;
+import com.springbackend.webservice.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 
 @Service
@@ -27,5 +30,20 @@ public class CategoryService {
 	
 	public Category insert(Category obj) {
 		return repository.save(obj);
+	}
+	
+	public Category update(Long id, Category obj) {
+		try {
+			Category entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	
+	private void updateData(Category entity, Category obj) {
+		entity.setName(obj.getName());
 	}
 }
